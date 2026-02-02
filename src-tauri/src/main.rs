a716865fcf9ba7b284cf0f3e12b2c6c5d94cafab
ramizloki <r.loki@masoft.ch>
2026-02-02 11:23:25 +0100

    feat(updater): release workflow and update config

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -43,6 +43,7 @@ fn main() {
                 .build(),
         )
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![print_raw])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
