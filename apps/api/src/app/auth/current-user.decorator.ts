3b8162631652ef3099806571538941b048b66d13
ramizloki <r.loki@masoft.ch>
2026-02-09 23:57:42 +0100

    fix(api): define authenticated user interface to avoid any type

--- apps/api/src/app/auth/current-user.decorator.ts
+++ apps/api/src/app/auth/current-user.decorator.ts
@@ -1,9 +1,12 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
+import { AuthenticatedUser } from './interfaces/authenticated-user.interface';
 
-export const CurrentUser = createParamDecorator((data: keyof any, ctx: ExecutionContext) => {
-  const request = ctx.switchToHttp().getRequest<Request & { user: any }>();
-  const user = request.user;
+export const CurrentUser = createParamDecorator(
+  (data: keyof AuthenticatedUser, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest<Request & { user: AuthenticatedUser }>();
+    const user = request.user;
 
-  return data ? user?.[data] : user;
-});
+    return data ? user?.[data] : user;
+  },
+);
