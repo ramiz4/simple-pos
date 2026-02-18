961060a4978eeb05b2a4c967c0b16a4e6faee4f4
ramizloki <r.loki@masoft.ch>
2026-02-18 09:01:16 +0100

    fix(api-e2e): set axios baseURL outside of setup function

--- apps/api-e2e/src/support/test-setup.ts
+++ apps/api-e2e/src/support/test-setup.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
+const host = process.env['HOST'] ?? 'localhost';
+const port = process.env['PORT'] ?? '3000';
+axios.defaults.baseURL = `http://${host}:${port}`;
+
 export default async function () {
   // Configure axios for tests to use.
-  const host = process.env['HOST'] ?? 'localhost';
-  const port = process.env['PORT'] ?? '3000';
-  axios.defaults.baseURL = `http://${host}:${port}`;
 }
